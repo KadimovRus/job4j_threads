57fea5ea7379851751d1491dc6641b89a8a62c1d
Rustam Kadimov <kadim_r@mail.ru>
2023-08-07 10:50:55 +0500

    3. Состояние нити.

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,21 +3,22 @@ package ru.job4j.concurrent;
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
-                () -> {
-                }
+                () -> System.out.println(Thread.currentThread().getName())
         );
         Thread second = new Thread(
-                () -> {
-                }
+                () -> System.out.println(Thread.currentThread().getName())
         );
         first.start();
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.printf("%s %s%n", first.getState(), first.getName());
-        }
         second.start();
-        while (second.getState() != Thread.State.TERMINATED) {
-            System.out.printf("%s %s%n", second.getState(), second.getName());
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            if (first.getState() != Thread.State.TERMINATED) {
+                System.out.printf("%s %s%n", first.getState(), first.getName());
+            }
+            if (second.getState() != Thread.State.TERMINATED) {
+                System.out.printf("%s %s%n", second.getState(), second.getName());
+            }
         }
+
         System.out.printf("%s %s%n", first.getState(), first.getName());
         System.out.printf("%s %s%n", second.getState(), second.getName());
         System.out.println("Работа завершена");
