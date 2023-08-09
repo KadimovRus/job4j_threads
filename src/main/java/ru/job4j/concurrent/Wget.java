a4a6e8267b54f69df1809d55636c053f3e85f1d2
Rustam Kadimov <kadim_r@mail.ru>
2023-08-09 11:28:48 +0500

    8. Скачивание файла с ограничением.

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,19 +1,66 @@
 package ru.job4j.concurrent;
 
-public class Wget {
-    public static void main(String[] args) {
-        Thread thread = new Thread(
-                () -> {
-                    for (int i = 0; i <= 100; i++) {
-                        System.out.print("\rLoading : " + i  + "%");
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }
-        );
-        thread.start();
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        var startAt = System.currentTimeMillis();
+        var file = new File("tmp.xml");
+        try (var in = new URL(this.url).openStream();
+             var out = new FileOutputStream(file)) {
+            System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
+            var dataBuffer = new byte[this.speed];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
+                var downloadAt = System.nanoTime();
+                out.write(dataBuffer, 0, bytesRead);
+                System.out.println("Read " + this.speed + " bytes : " + (System.nanoTime() - downloadAt) + " nano.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            System.out.println(Files.size(file.toPath()) + " bytes");
+        } catch (IOException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static boolean urlValidator(String url) {
+        try {
+            new URL(url).toURI();
+            return true;
+        } catch (URISyntaxException exception) {
+            return false;
+        } catch (MalformedURLException exception) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        if (urlValidator(url)) {
+            Thread wget = new Thread(new Wget(url, speed));
+            wget.start();
+            wget.join();
+        } else {
+            System.out.println("Invalid URL: " + url);
+        }
     }
 }
