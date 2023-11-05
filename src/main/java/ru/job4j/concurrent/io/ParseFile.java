e1798021f533a8fa8e86e986bd19a447d5d83ead
Rustam Kadimov <kadim_r@mail.ru>
2023-11-05 12:19:37 +0500

    Visibility. Общий ресурс вне критической секции fix #2

--- src/main/java/ru/job4j/concurrent/io/ParseFile.java
+++ src/main/java/ru/job4j/concurrent/io/ParseFile.java
@@ -18,7 +18,7 @@ public final class ParseFile<T> implements GetContent<T> {
         try (InputStream i = new FileInputStream(file)) {
             StringBuilder output = new StringBuilder();
             Integer data;
-            while ((data = i.read()) != 0) {
+            while ((data = i.read()) != -1) {
                 if (filter.test((T) data)) {
                     output.append(data);
                 }
