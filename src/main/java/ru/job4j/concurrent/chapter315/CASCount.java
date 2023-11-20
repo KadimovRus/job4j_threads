47f39b8202b7e51c87c728ca24ac1c9e6420dfaa
Rustam Kadimov <kadim_r@mail.ru>
2023-11-20 09:37:18 +0500

    CAS - операции

---
+++ src/main/java/ru/job4j/concurrent/chapter315/CASCount.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent.chapter315;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicInteger count = new AtomicInteger();
+
+    public void increment() {
+        int temp;
+        int ref;
+
+        do {
+           ref = count.intValue();
+           temp = ref + 1;
+        } while (!count.compareAndSet(ref, temp));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
