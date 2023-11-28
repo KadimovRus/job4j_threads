be2dcbdd2526f25af8c4673470999d0e1cf70663
Rustam Kadimov <kadim_r@mail.ru>
2023-11-28 11:21:56 +0500

    CAS - операции add test

--- src/test/java/ru/job4j/concurrent/chapter315/CASCountTest.java
+++ src/test/java/ru/job4j/concurrent/chapter315/CASCountTest.java
@@ -2,6 +2,10 @@ package ru.job4j.concurrent.chapter315;
 
 import org.junit.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CASCountTest {
@@ -16,4 +20,24 @@ public class CASCountTest {
         assertEquals(2, counter.get());
     }
 
+    @Test
+    public void testIncrementAndGetMultiThread() throws InterruptedException {
+        CASCount counter = new CASCount();
+        int threadsNum = 10;
+        int iterations = 100;
+
+        ExecutorService service = Executors.newFixedThreadPool(threadsNum);
+        for (int i = 0; i < threadsNum; i++) {
+            service.submit(() -> {
+                for (int j = 0; j < iterations; j++) {
+                    counter.increment();
+                }
+            });
+        }
+
+        service.shutdown();
+        service.awaitTermination(60, TimeUnit.SECONDS);
+
+        assertEquals(threadsNum * iterations, counter.get());
+    }
 }
